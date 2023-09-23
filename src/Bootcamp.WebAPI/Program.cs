cd3f0c7e1bc96e7026e7a134e3ef272d2d162004
anishar <anishar@techkraftinc.com>
2023-09-23 19:11:19 +0545

    chore: setup clean architecture layer

--- src/Bootcamp.WebAPI/Program.cs
+++ src/Bootcamp.WebAPI/Program.cs
@@ -1,14 +1,6 @@
-using Bootcamp.WebAPI.Data;
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-//configure db
-builder.Services.AddDbContext<BootcampDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -16,7 +8,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
--- src/Bootcamp.WebAPI/Services/DateTimeProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using Bootcamp.WebAPI.Common.Interface.Services;
-
-namespace Bootcamp.WebAPI.Services
-{
-    public class DateTimeProvider : IDateTimeProvider
-    {
-        public DateTime UtcNow => DateTime.UtcNow;
-    }
-}
